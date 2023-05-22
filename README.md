70c1f356675ae89b733d38e6515a1fe1b47223c4
Antony <115037549+antonysantos71@users.noreply.github.com>
2023-05-22 15:19:07 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+COMO RODAR O PROJETO BAIXADO
+Instalar todas as denpendecias indicadas pelo package.json 
+### npm install
+
+SEQUEMCIAL PARA CRIAR O PROJETO
+Criar o arquivo package
+### npm init -y 
+
+Gerencia as requisições. rotas e URLS , entre outras funcionalidades
+### npm install express
+
+Instalar o modulo para reiniciar o servidor sempre que houver alteração no código fonte, g significa globalmente
+### nodemon app.js
+
+Acessa o projeto no navegador
+### http://localhost:8080/
+
---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const pessoa = [];
+
+
+app.get('/contato',  (req, res) => {
+  res.json(pessoa);
+});
+
+app.get('/contato/:_id',  (req, res) => {
+  const {_id} = req.params;
+  return res.json({
+    user: pessoa[_id]
+  });
+});
+
+app.post('/contato',  (req, res) => {
+  const {name , age} = req.body;
+  const id = pessoa.length + 1;
+  pessoa.push({
+    name,
+    age
+  });
+  return res.json({
+    mensage: "usuario adicinado",
+    user: {
+      name,
+      age
+    }
+  });
+});
+
+app.put('/contato/:_id',  (req, res) => {
+  const {_id} = req.params;
+  const {name , age} = req.body;
+  pessoa[_id] = {name, age};
+  return res.json({
+    mensage: "usuario atualizado",
+    user: {
+      name,
+      age
+    }
+  })
+});
+
+app.delete('/contato/:id',  (req, res) => {
+  const {id} = req.params;
+  pessoa.splice(id, 1);
+  return res.json({
+    mensage: "usuario deletado",
+  })
+});
+
+app.listen(8080,() =>{
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080/ ");
+});
